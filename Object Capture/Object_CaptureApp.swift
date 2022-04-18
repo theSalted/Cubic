816c9c4e575ddb7079bc1d071b9fbaad5fea0c7b
Yuhao <yuhao.chen2000@icloud.com>
2022-04-18 15:14:51 -0700

    Initial Commit

---
+++ Object Capture/Object_CaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Object_CaptureApp.swift
+//  Object Capture
+//
+//  Created by Yuhao Chen on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Object_CaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Object CaptureUITests/Object_CaptureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Object_CaptureUITestsLaunchTests.swift
+//  Object CaptureUITests
+//
+//  Created by Yuhao Chen on 4/18/22.
+//
+
+import XCTest
+
+class Object_CaptureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
